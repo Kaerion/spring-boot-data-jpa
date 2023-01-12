b802e125146619ec968ac732273cbf6718711500
Jose Antonio Quero <jquero89@gmail.com>
2023-01-12 12:16:27 +0100

    First Commit

---
+++ src/main/java/com/nombreempresa/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,27 @@
+package com.nombreempresa.springboot.app.models.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nombreempresa.springboot.app.models.entity.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+
+	@PersistenceContext
+	private EntityManager em; // Consultas jpa a la clase entity, no a la tabla
+
+	@Override
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+}
---
+++ src/main/java/com/nombreempresa/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.nombreempresa.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.nombreempresa.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao {
+
+	public List<Cliente> findAll();
+}
