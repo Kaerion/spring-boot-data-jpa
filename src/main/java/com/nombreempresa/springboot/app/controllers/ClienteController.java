145c51aff24bab22b4f619c8000b33d32f309af3
Kaerion <jquero89@gmail.com>
2023-01-31 14:17:09 +0100

    Errors managing and styles

--- src/main/java/com/nombreempresa/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/nombreempresa/springboot/app/controllers/ClienteController.java
@@ -5,11 +5,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.nombreempresa.springboot.app.models.dao.IClienteDao;
 import com.nombreempresa.springboot.app.models.entity.Cliente;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ClienteController {
@@ -28,12 +31,17 @@ public class ClienteController {
 	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
 		model.put("cliente", cliente);
-		model.put("titulo", "Formulario de cliente");
+		model.put("titulo", "Formulario de Cliente");
 		return "form";
 	}
 
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Cliente");
+			return "form";
+		}
+
 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}
--- src/main/java/com/nombreempresa/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/nombreempresa/springboot/app/models/entity/Cliente.java
@@ -3,15 +3,20 @@ package com.nombreempresa.springboot.app.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "clientes")
@@ -21,18 +26,23 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotEmpty
+	@Size(min = 4, max = 20)
 	private String nombre;
+
+	@NotEmpty
+	@Size(min = 4, max = 20)
 	private String apellido;
+
+	@NotEmpty
+	@Email
 	private String email;
 
+	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
-
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
 
 	public Long getId() {
 		return id;
