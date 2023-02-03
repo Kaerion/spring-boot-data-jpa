ae66324bad16a31728c92babaf22bee6fbb67795
Kaerion <jquero89@gmail.com>
2023-02-03 12:51:28 +0100

    Update, Delete, Datasource changes and CrudRepository use

--- src/main/java/com/nombreempresa/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/nombreempresa/springboot/app/models/dao/IClienteDao.java
@@ -1,12 +1,9 @@
 package com.nombreempresa.springboot.app.models.dao;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 import com.nombreempresa.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
 
-	public List<Cliente> findAll();
-
-	public void save(Cliente cliente);
 }
